444b2835a21358915e1579d2359b79b58c16a42e
pavelPataP88 <pavelpatap88@gmail.com>
2026-08-12 16:45:39 +0200

    Add v0.3.0 context benchmark

--- patap/__init__.py
+++ patap/__init__.py
@@ -5,4 +5,4 @@ from .memory import PATAPMemory
 from .models import State
 
 __all__ = ["PATAP", "PATAPMemory", "State", "CycleError", "UnknownStateError", "ValidationError"]
-__version__ = "0.2.1"
+__version__ = "0.3.0"
